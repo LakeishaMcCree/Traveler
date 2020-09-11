a62162f737590d03aabe49ab7a4a5d37158d7b40
github username <lmccree215@icloud.comgit config --global user.name LakeishaMcCree>
2020-09-11 00:47:48 -0400

    updated the cities controller to include most_visited and highly_rated.

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -1,3 +1,38 @@
 class CitiesController < ApplicationController
+
+    before_action :require_login
     
+    def index
+        @city = City.all.order(:name)
+    end
+
+    def show
+        @city = City.find(params[:id])
+    end
+
+    def most_visited
+        @city = City.most_visited.first
+    end
+
+    def highly_rated
+        @cities = City.highly_rated
+    end
+
+    def new
+        @city = City.new
+    end
+
+    def create
+        @city = City.create(city_params)
+        if @city.save
+            redirect_to cities_path, :flash => { :success => "You've added this city!" }
+        else
+            render :new
+        end
+    end
+
+    private
+    def city_params
+        params.require(:city).permit(:name, :country_id, country_name:[:country])
+
 end
