ccbe512ee22138379b6c10b1be3f05feb4937f00
github username <lmccree215@icloud.comgit config --global user.name LakeishaMcCree>
2020-09-11 00:50:48 -0400

    added the countries controller

---
+++ app/controllers/countries_controller.rb
@@ -0,0 +1,13 @@
+class CountriesController < ApplicationController
+    before_action :require_login
+
+    def index
+        @countries = Country.all.order(:name)
+    end
+
+    def show
+        @country = Country.find(params[:id])
+    end
+
+    
+end
